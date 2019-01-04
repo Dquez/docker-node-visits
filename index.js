d71d9d322b3017a30f1bc2678b960cef57b2d183
Dariell Vasquez <dariellv@yahoo.com>
2019-01-04 15:30:35 -0500

    working container with docker-compose

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+});
+client.set('visits', 0);
 
 app.get('/', (req, res)=>{
-    client.get('vists', (err, visits)=>{
-        res.send(' Number of vists ' + visits);
+    client.get('visits', (err, visits)=>{
+        if (err) throw err;
+        client.set('visits', parseInt(visits) + 1)
+        res.send(' Number of visits ' + visits);
     })
-    client.set('vists', parseInt(vists) + 1)
+   
 })
 
 app.listen(8081, ()=>{
-    console.log('Listening on port 8081')
+    console.log('Listening on port 4001')
 })
